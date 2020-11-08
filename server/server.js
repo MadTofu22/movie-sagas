3b59d4527b121126df46c679d519be174a612b6a
MadTofu22 <stutler.tom@gmail.com>
2020-11-07 21:52:15 -0600

    added joint router and query to get a single movies genre tags

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const movieRouter = require('./routes/movie.router.js')
-const genreRouter = require('./routes/genre.router.js')
+const movieRouter = require('./routes/movie.router.js');
+const genreRouter = require('./routes/genre.router.js');
+const jointRouter = require('./routes/joint.router.js');
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -11,7 +12,8 @@ app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
 app.use('/api/movie', movieRouter);
-app.use('/api/genre', genreRouter)
+app.use('/api/genre', genreRouter);
+app.use('/api/joint', jointRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
